ca9d9e9f6d83e68ba332d4c989a3c03e93c20af4
Jacob <jacobboogiebear@icloud.com>
2023-05-05 10:04:50 -0500

    Removed 32bit support and updated rust to nightly build.

--- scripts/build-i686-pc-windows-msvc.ps1
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023 Jacob Allen Morris
-# 
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-if (!(Test-Path -Path "output")) {
-	New-Item -Path "output" -ItemType Directory
-}
-Copy-Item -Path "target/i686-pc-windows-msvc/release/bfc.exe" -Destination "output/bfc-i686-pc-windows-msvc.exe"
